87401234fd89a839f740bd31643904979ced7aba
CSComarch <4men@vp.pl>
2023-10-18 15:09:44 +0200

    update

--- Generatory_zad2.py
+++ Generatory_zad2.py
@@ -8,18 +8,25 @@ print(next(generator_1))
 
 
 def generator2():  # tworzenie nieskończonego generaota bez koneczności obciążania systemu
-    i = 1
+    i = 1  # licznik jest inicjowany tylko przy pierwszym użyciu generatora
     while True:
         yield i * i
         i += 1
 
 
 def fibo():
-    a,b = 0, 1
-    while True:
-        yield b
+    a,b = 0, 1  # zmienne są inicjowany tylko raz przy pierwszym wejścio do generatora
+    while True:  # pętla jest niby nieskończona, ale słówko yield przerywa tą pętlę
+        yield b  # słówko yield służy do zapamiętania wartości i wyjścia z pętli
         a, b = b, a + b
 
+def fibo_with_index(n):
+    a, b = 0, 1
+    for idx in range(n):
+        yield idx, a
+        a, b = b, a + b
+
+# x = idx, a - tupla(krotka)
 
 
 print("----------------")
@@ -44,3 +51,34 @@ print('------------------')
 print(next(fib))
 print(next(fib))
 print(next(fib))
+
+# przerwa od 14:48 do 14:55
+for i in fibo_with_index(10):
+    print(f"element: {i} ")
+
+for i, n in fibo_with_index(10):
+    print(f"pozycja: {i}: {n}")
+
+    print("-----------------------")
+
+    # kolejny generator
+    def counter(start=0):
+        n = start
+        while True:
+            result = yield n
+            print(result)
+            if result == 'q': # sterowanie generatorem
+                break
+            n += 1
+
+try:
+    c = counter(10)
+    print(next(c))
+    print(next(c))
+    print(next(c))
+    print(next(c))
+    print(c.send('OK'))
+    print(c.send('q'))
+    print(next(c)) # błąd StopIteration
+except StopIteration:
+    print("generator zakończył działanie")
