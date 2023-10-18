76450dc6c07304e6d86343ff92c0497a0ea4fbb4
CSComarch <4men@vp.pl>
2023-10-18 14:01:27 +0200

    update

--- dekoratory2.py
+++ dekoratory2.py
@@ -5,7 +5,15 @@ def uppercase_decorator(function):
 
     return wrapper
 
+def bold_decorator(function):
+    def wrapper():
+        result = function()
+    #    return f"<b>{result}</b>"  # nie działa na konsoli Windows
+        return f"\033[1m" + "Tekst" + "\033[0m"  # to działa
 
+    return wrapper
+
+@bold_decorator
 @uppercase_decorator
 def greeting():
     return "hello, world!"
