7ec7fbfa6fc12dd08acedbdcc6d0c93610592cb5
CSComarch <4men@vp.pl>
2023-10-18 13:47:39 +0200

    update

---
+++ dekoratory.py
@@ -0,0 +1,20 @@
+# dekoratory - opakowanie jednej funkcji dodatkowymi cechami
+
+def dekor(funk):
+    def wew():
+        print("Dekorujemy")
+        return funk()
+
+    return wew()
+
+
+@dekor  # użycie dekoratora na naszej funkcji
+def hej():
+    print("Hej")
+
+
+
+hej()
+
+
+Nie Działa
