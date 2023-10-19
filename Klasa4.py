b073a669160b8bb254f5694324f634e84713cae4
CSComarch <4men@vp.pl>
2023-10-19 11:46:25 +0200

    update

--- Klasa4.py
+++ Klasa4.py
@@ -25,6 +25,23 @@ class Suplier(Contact):
         print(f"{order} zamówiono od {self.name}")
 
 
+class Friend(Suplier, Contact):
+    """
+    dziedziczenie po wielu klasach, po klasie Suplier i Contact
+    """
+    def __init__(self, name, email, phone):
+        super().__init__(name, email)
+        self.phone = phone
+
+    def order(self, order):
+        # super(Friend, self).order(order)  # wymuszenie użycia metody z klasy wyższej
+        # Suplier.order(self, order)
+        print("To ja", self.name, f"{order}")
+
+    def __repr__(self):
+        return f'{self.name!r}, {self.email!r}, {self.phone!r}'
+
+
 cl = ContactList()
 print(cl)
 cl.append("e")
@@ -36,3 +53,13 @@ print('------------')
 s = Suplier("Radek", "radek@wp.pl")
 s.order("kawa")  # kawa zamówiono od Radek
 print(s.all_contacts)  # ['Adam', 'adam@wp.pl', 'Radek', 'radek@wp.pl']
+print('--------------')
+f = Friend("Jarek", "jarek@wp.pl", "123456")
+print(f)
+# funkcja do sprawdzania po czym dziedziczy dana klasa
+print(Friend.__mro__)  # (<class '__main__.Friend'>, <class '__main__.Suplier'>, <class '__main__.Contact'>, <class 'object'>)
+# powyżej sprawdzanie kolejności dziedziczenia, kolejności rozwiązaywania nazw
+f.order("herbata")
+print(f)  # 'Jarek', 'jarek@wp.pl', '123456'
+print(f.all_contacts)  # ['Adam', 'adam@wp.pl', 'Radek', 'radek@wp.pl', 'Jarek', 'jarek@wp.pl', '123456']
+
